c1ea9ffd5b6b955a46b9a3d70879c1dc6162a5b8
Pritam Tirpude <pritam.tirpude9601@gmail.com>
2023-10-22 21:26:41 +0530

    homepage and question card ui layout changes

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -6,51 +6,51 @@ import RenderTag from "./RenderTag";
 const RightSidebar = () => {
   const hotQuestions = [
     {
-      _id: 1,
+      _id: "1",
       title: "How do I use express as a custom server in NextJS?",
     },
     {
-      _id: 2,
+      _id: "2",
       title: "Cascading Deletes in SQLAchemy?",
     },
     {
-      _id: 3,
+      _id: "3",
       title: "How to Perfectly center a div with tailwind css?",
     },
     {
-      _id: 4,
+      _id: "4",
       title:
         "Best practices for data fetching in  a Next.js application with Server-side Rendering (SSR)?",
     },
     {
-      _id: 5,
+      _id: "5",
       title: "Redux Toolkit Not Updating state as Expected?",
     },
   ];
 
   const popularTags = [
     {
-      _id: 1,
+      _id: "1",
       name: "javascript",
       totalQuestions: 5,
     },
     {
-      _id: 2,
+      _id: "2",
       name: "next",
       totalQuestions: 4,
     },
     {
-      _id: 3,
+      _id: "3",
       name: "react",
       totalQuestions: 5,
     },
     {
-      _id: 4,
+      _id: "4",
       name: "vue",
       totalQuestions: 2,
     },
     {
-      _id: 5,
+      _id: "5",
       name: "redux",
       totalQuestions: 10,
     },
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,18 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const formatAndDivideNumber = (num: number): string => {
+  if (num >= 1000000) {
+    const formattedNum = (num / 1000000).toFixed(1);
+    return `${formattedNum}M`;
+  } else if (num >= 1000) {
+    const formattedNum = (num / 1000).toFixed(1);
+    return `${formattedNum}K`;
+  } else {
+    return num.toString();
+  }
+};
