ef814c9734614979c83012eb31e743a88da64bca
Pritam Tirpude <pritam.tirpude9601@gmail.com>
2023-11-02 21:36:30 +0530

    db connection and questions rendering on t homepage from the database

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import Question from "@/components/forms/Question";
+import { getUserById } from "@/lib/actions/user.action";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-const AskQuestionPage = () => {
-  return <div>AskQuestionPage</div>;
+const AskQuestionPage = async () => {
+  // const { userId } = auth();
+
+  const userId = "12345";
+
+  if (!userId) redirect("/sign-in");
+
+  const mongoUser = await getUserById({ userId });
+
+  return (
+    <div>
+      <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+      <div className="mt-9">
+        <Question mongoUserId={JSON.stringify(mongoUser._id)} />
+      </div>
+    </div>
+  );
 };
 
 export default AskQuestionPage;
